2a0e1efb66f140d120b88b4db127c0a81566b8ef
ginger cea <gingercea@gmail.com>
2023-07-28 22:07:14 -0400

    se creo Listado y fragmento agregar

---
+++ app/src/main/java/com/example/myapp2m6/presentation/FragmentAgregar.kt
@@ -0,0 +1,28 @@
+package com.example.myapp2m6.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.myapp2m6.R
+
+
+class FragmentAgregar : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_agregar, container, false)
+
+    }}
+
+
+
+
+
+
+
