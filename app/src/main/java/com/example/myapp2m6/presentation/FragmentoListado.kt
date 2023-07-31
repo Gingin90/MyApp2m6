64338932249d8a54e39408255914aba0b6928637
ginger cea <gingercea@gmail.com>
2023-07-31 19:27:27 -0400

    se creo recycler view, fragmet listado con binding lead data init lisitener

--- app/src/main/java/com/example/myapp2m6/presentation/FragmentoListado.kt
+++ app/src/main/java/com/example/myapp2m6/presentation/FragmentoListado.kt
@@ -5,56 +5,43 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import com.example.myapp2m6.R
+import com.example.myapp2m6.databinding.FragmentoListadoBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [FragmentoListado.newInstance] factory method to
- * create an instance of this fragment.
- */
 class FragmentoListado : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var binding : FragmentoListadoBinding
+    private val viewModel : ItemViewModel by activityViewModels()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragmento_listado, container, false)
+        binding =FragmentoListadoBinding.inflate(layoutInflater,container, false)
+
+    initRecyclerView()
+        loadData()
+        initListener()
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FragmentoListado.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FragmentoListado().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun initListener() {
+        TODO("Not yet implemented")
     }
+
+    private fun loadData() {
+        TODO("Not yet implemented")
+    }
+
+    private fun initRecyclerView() {
+        TODO("Not yet implemented")
+    }
+
+
 }
+
