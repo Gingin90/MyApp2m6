d9d2accaf0646af3f2d0bb4f664ed663d1ec5a15
ginger cea <gingercea@gmail.com>
2023-07-31 20:18:19 -0400

    se creo nav graph con su implementacion y dependecias

--- app/src/main/java/com/example/myapp2m6/presentation/FragmentAgregar.kt
+++ app/src/main/java/com/example/myapp2m6/presentation/FragmentAgregar.kt
@@ -1,24 +1,42 @@
 package com.example.myapp2m6.presentation
-
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.myapp2m6.R
+import androidx.fragment.app.activityViewModels
+import com.example.myapp2m6.databinding.FragmentAgregarBinding
 
 
 class FragmentAgregar : Fragment() {
 
+    private lateinit var binding : FragmentAgregarBinding
+    private val itemViewModel: ItemViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_agregar, container, false)
+        binding = FragmentAgregarBinding.inflate(layoutInflater, container, false)
 
-    }}
+        initListener()
+
+        return binding.root
+    }
+
+    private fun initListener() {
+     binding.btnGuardar.setOnClickListener{
+
+         val nombre = binding.editTextNombre.toString()
+         val cantidad = binding.editTextNombre.toString().toInt()
+         val precio = binding.editTextNombre.toString().toInt()
+
+         itemViewModel.insertItem(nombre, precio,cantidad)
+     }
+   }
+  }
+
 
 
 
